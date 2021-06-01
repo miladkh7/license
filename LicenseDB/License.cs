11de1df177965fadd8efa70ed30018a1de6052e2
Milad <milad_khaleghi@Live.com>
2021-06-01 13:09:10 +0430

    feat(License):Check Licence Key

---
+++ LicenseDB/License.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace LicenseDB
+{
+    class License
+    {
+        string _apiKey= "5e0c5fac2b88e41892f7dd511abb5469b1a09";
+        string _dataBaseName = "gholami-c537";
+        public License(string dataBaseName,string apiKey)
+        {
+            this._dataBaseName = dataBaseName;
+            this._apiKey = apiKey;
+        }
+        public void CheckHardWareID(string dataBase,string license)
+        {
+            string searchQuary =string.Format(@"{{""key"":""{0}""}}",license);
+            string quary = string.Format(@"https://{0}.restdb.io/rest/license?q={1}", dataBase, searchQuary);
+            Console.WriteLine(quary);
+            var client = new RestClient(quary);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-api-key", _apiKey);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+
+    }
+}
--- LicenseDB/Program.cs
+++ LicenseDB/Program.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using LicenseDB;
 namespace LicenseDB
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string apiKey = "5e0c5fac2b88e41892f7dd511abb5469b1a09";
+            string dataBaseName = "gholami-c537";
+            License myLisence = new License(dataBaseName, apiKey);
+            myLisence.CheckHardWareID(dataBaseName,"123");
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello Worlds!");
+            Console.WriteLine("Hello World!");
+
+            
+
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
