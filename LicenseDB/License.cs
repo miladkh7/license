36e98f3eddf5daa78db1edf6f8e3b16a5c6e19f0
milad <milad_khaleghi@Live.com>
2021-06-03 07:03:29 +0430

    feat(license):add collection name

--- LicenseDB/License.cs
+++ LicenseDB/License.cs
@@ -20,6 +20,7 @@ namespace LicenseDB
 
         string _apiKey= "5e0c5fac2b88e41892f7dd511abb5469b1a09";
         string _dataBaseName = "gholami-c537";
+        string _collectionName;
         string _id;
         string  licenseKey;
         string HWID = new DeviceIdBuilder()
@@ -28,17 +29,18 @@ namespace LicenseDB
         .AddMotherboardSerialNumber()
         .AddSystemDriveSerialNumber()
         .ToString();
-        public License(string dataBaseName,string apiKey)
+        public License(string dataBaseName,string CollectionName,string apiKey)
         {
             this._dataBaseName = dataBaseName;
+            this._collectionName = CollectionName;
             this._apiKey = apiKey;
             
         }
-        public string CheckHardWareID(string dataBase,string license)
+        public string CheckHardWareID(string dataBase,string collection,string license)
         {
             Console.WriteLine(this.HWID);
             string searchQuary =string.Format(@"{{""key"":""{0}""}}",license);
-            string quary = string.Format(@"https://{0}.restdb.io/rest/license?q={1}", dataBase, searchQuary);
+            string quary = string.Format(@"https://{0}.restdb.io/rest/{1}?q={2}", dataBase,collection, searchQuary);
             Console.WriteLine(quary);
             var client = new RestClient(quary);
             client.Timeout = -1;
--- LicenseDB/Program.cs
+++ LicenseDB/Program.cs
@@ -12,9 +12,10 @@ namespace LicenseDB
         {
             string apiKey = "5e0c5fac2b88e41892f7dd511abb5469b1a09";
             string dataBaseName = "gholami-c537";
-            License myLisence = new License(dataBaseName, apiKey);
+            string collection = "license";
+            License myLisence = new License(dataBaseName, collection, apiKey);
             //60b5e06f6a5d621100014665
-            myLisence.CheckHardWareID(dataBaseName,"123");
+            myLisence.CheckHardWareID(dataBaseName, collection,"123");
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
